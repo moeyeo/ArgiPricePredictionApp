625111d4455356e6b175e2c5a63974ead23c8a53
moeyeo <eye_kanarat@hotmail.com>
2019-03-27 00:29:46 +0700

    date picker on planning activity.

--- app/src/main/java/com/moeyeo/argipredictionapp/PlanActivity.java
+++ app/src/main/java/com/moeyeo/argipredictionapp/PlanActivity.java
@@ -1,7 +1,15 @@
 package com.moeyeo.argipredictionapp;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class PlanActivity extends AppCompatActivity {
 
@@ -9,5 +17,33 @@ public class PlanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.plan_layout);
+
+        final Calendar myCalendar = Calendar.getInstance();
+
+        final EditText edittext= (EditText) findViewById(R.id.startDate);
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                myCalendar.set(Calendar.YEAR, i);
+                myCalendar.set(Calendar.MONTH, i1);
+                myCalendar.set(Calendar.DAY_OF_MONTH, i2);
+                String myFormat = "dd/MM/yy"; //In which you need put here
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+                edittext.setText(sdf.format(myCalendar.getTime()));
+            }
+
+        };
+        edittext.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(PlanActivity.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
     }
 }
