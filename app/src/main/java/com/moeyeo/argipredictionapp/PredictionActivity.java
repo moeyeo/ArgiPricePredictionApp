06bff6702677390611e4a10ca2d659f5462507c1
moeyeo <eye_kanarat@hotmail.com>
2019-03-27 03:05:02 +0700

    add searchView on predict activity.

--- app/src/main/java/com/moeyeo/argipredictionapp/PredictionActivity.java
+++ app/src/main/java/com/moeyeo/argipredictionapp/PredictionActivity.java
@@ -2,15 +2,39 @@ package com.moeyeo.argipredictionapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.app.SearchManager;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import static java.security.AccessController.getContext;
 
 public class PredictionActivity extends AppCompatActivity {
 
+    SearchView sv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.prediction_layout);
+
+        sv = (SearchView) findViewById(R.id.searchView);
+        sv.setOnQueryTextListener(new OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                Toast.makeText(PredictionActivity.this, s, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+        //System.out.print(sv.getQuery());
 
     }
 
